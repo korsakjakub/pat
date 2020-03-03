1c80933de792197034683ac7f283cc16ec037654
Jakub Korsak <korsakuba@gmail.com>
2020-03-03 12:35:56 +0100

    NewTable function

--- table.go
+++ table.go
@@ -1,7 +1,21 @@
 package main
 
+import "errors"
+
 type Table struct {
-	Players  []Player
-	Deck     Deck
-	Settings Settings
+	Players    []Player
+	TableCards Deck
+	Deck       Deck
+	Settings   Settings
+}
+
+func NewTable(p []Player, s Settings) (*Table, error) {
+	var err error
+	if len(p) < 1 {
+		err = errors.New("You need players in a table.")
+	}
+	return &Table{Players: p,
+		TableCards: Deck{},
+		Deck:       *NewDeck(),
+		Settings:   s}, err
 }
--- table_test.go
+++ table_test.go
@@ -1,1 +1,15 @@
 package main
+
+import "testing"
+
+func TestNewTable(t *testing.T) {
+	p1, _ := NewPlayer("jakub", 1000.0, Settings{})
+	p2, _ := NewPlayer("ola", 1001.20, Settings{})
+	p := []Player{*p1, *p2}
+
+	if table, err := NewTable(p, Settings{}); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(table)
+	}
+}
